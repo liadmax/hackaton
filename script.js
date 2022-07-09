667b19c61a3e5f5366c337afb6878ae2716b9518
davidrapa <davidrapaport01@gmail.com>
2022-07-09 09:51:38 +0300

    dragable

--- script.js
+++ script.js
@@ -19,63 +19,16 @@ function myFunction() {
     }
   }
 
-  /* The dragging code for '.draggable' from the demo above
- * applies to this demo as well so it doesn't have to be repeated. */
-
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-    // only accept elements matching this CSS selector
-    accept: '#yes-drop',
-    // Require a 75% element overlap for a drop to be possible
-    overlap: 0.75,
-    accept: '#yes-drop2',
-    // Require a 75% element overlap for a drop to be possible
-    overlap: 0.75,
-    accept: '#yes-drop3',
-    // Require a 75% element overlap for a drop to be possible
-    overlap: 0.75,
+  var data = {"total":0,"rows":[]};
+  var totalCost = 0;
   
-    // listen for drop related events:
   
-    ondropactivate: function (event) {
-      // add active dropzone feedback
-      event.target.classList.add('drop-active')
-    },
-    ondragenter: function (event) {
-      var draggableElement = event.relatedTarget
-      var dropzoneElement = event.target
-  
-      // feedback the possibility of a drop
-      dropzoneElement.classList.add('drop-target')
-      draggableElement.classList.add('can-drop')
-      draggableElement.textContent = 'Dragged in'
-    },
-    ondragleave: function (event) {
-      // remove the drop feedback style
-      event.target.classList.remove('drop-target')
-      event.relatedTarget.classList.remove('can-drop')
-      event.relatedTarget.textContent = 'Dragged out'
-    },
-    ondrop: function (event) {
-      event.relatedTarget.textContent = 'Dropped'
-    },
-    ondropdeactivate: function (event) {
-      // remove active dropzone feedback
-      event.target.classList.remove('drop-active')
-      event.target.classList.remove('drop-target')
+  $( "#draggable1" ).draggable();
+  $( "#draggable2" ).draggable();
+  $( "#draggable3" ).draggable();
+  $( "#draggable4" ).draggable();
+  $( "#droppable" ).droppable({
+    drop: function() {
+      alert( "dropped" );
     }
-  })
-  
-  interact('.drag-drop')
-    .draggable({
-      inertia: true,
-      modifiers: [
-        interact.modifiers.restrictRect({
-          restriction: 'parent',
-          endOnly: true
-        })
-      ],
-      autoScroll: true,
-      // dragMoveListener from the dragging demo above
-      listeners: { move: dragMoveListener }
-    })
+  });
